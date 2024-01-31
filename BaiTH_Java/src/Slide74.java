17346ec4bb24fe385839afba1e4197b01fa7acbd
ChuVChien <chien59941@gmail.com>
2024-01-31 21:51:59 +0700

    Slide 74, 81, 90

---
+++ BaiTH_Java/src/Slide74.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Slide74 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Nhap vao so phan tu cua mang: ");
+            n =sc.nextInt();
+        } while (n<0);
+        int a[] =  new int[n];
+        for(int i = 0; i < n; i++){
+            System.out.print("Nhap phan tu thu " + i + " : ");
+            a[i] = sc.nextInt();
+        } 
+        int s = 0;
+        for(int i = 0; i < n; i++){
+            if(a[i]%2==0)
+            {s = s + a[i];}
+        }
+        System.out.print("Tong : " + s);
+    }
+}
---
+++ BaiTH_Java/src/Slide90.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Slide90 {
+    public static void main(String[] args) {
+    String chuoi;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Nhap vao 1 chuoi: ");
+    chuoi = sc.nextLine();
+    for(int i = 0; i < chuoi.length(); i++)
+        {
+            System.out.println(chuoi.charAt(i));
+        }
+    }
+}
