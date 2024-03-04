a80c8cea202cde20cc88351064b704f05b4eeebe
ChuVChien <chien59941@gmail.com>
2024-03-04 16:36:22 +0700

    Slide 16,17,21,22,27,29,31 chuong 3

---
+++ BaiTH_Java/src/HinhTron1.java
@@ -0,0 +1,13 @@
+public class HinhTron1 {
+    public final float PI = 3.14f;
+    public float r;
+    public float cv;
+    public float dt;
+
+    public void tinhchuVi()
+    {
+        float r = 10;
+        this.r = r;
+        cv = 2*r*PI;
+    }
+}
---
+++ BaiTH_Java/src/app_test.java
@@ -0,0 +1,11 @@
+public class app_test {
+    public static void main(String[] args) {
+        HinhTron  HT1 = new HinhTron();
+        System.out.print("Thong so hinh tron 1");
+        HT1.nhapBK();
+        HT1.tinhChuVi();
+        HT1.inChuVi();
+        HT1.tinhDienTich();
+        HT1.inDienTich();
+    }
+}
