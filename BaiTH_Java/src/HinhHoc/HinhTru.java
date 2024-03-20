c238c025642c87f7c62d02773097084801f62b33
ChuVChien <chien59941@gmail.com>
2024-03-21 06:44:55 +0700

    Slile 54

---
+++ BaiTH_Java/src/HinhHoc/HinhTru.java
@@ -0,0 +1,24 @@
+
+package HinhHoc;
+import java.util.Scanner;
+
+public class HinhTru extends HinhTron {
+    public float chieuCao;
+
+    public HinhTru() {
+        ten = "Hinh Tru";
+    }
+
+    public void nhapChieuCao() {
+        nhapBanKinh();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap chieu cao: ");
+        chieuCao = sc.nextFloat();
+    }
+
+    public void tinhTheTich() {
+        tinhDienTich();
+        theTich = chieuCao * dienTich;
+    }
+}
+
---
+++ BaiTH_Java/src/HinhHoc/HinhVuong.java
@@ -0,0 +1,16 @@
+package HinhHoc;
+
+import java.util.Scanner;
+
+public class HinhVuong extends HinhChuNhat {
+    public HinhVuong()
+    {
+        ten = "Hinh vuong";
+    }
+    public void nhapCanh()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap chieu canh: ");
+        dai = rong = sc.nextFloat();
+    }
+}
